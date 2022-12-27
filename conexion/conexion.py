605773b044acdd7300e1109b2e04beb73f73cb70
edube19 <92874291+edube19@users.noreply.github.com>
2022-12-27 10:12:25 -0500

    Add files via upload

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -3,14 +3,12 @@ from re_excel import *
 def conexion(admin_name):
     engine = create_engine(f'mysql://root:admin@localhost/{admin_name}')
     # Obtiene información sobre la estructura de la base de datos
-    
-    
     return engine
 
 def consulta_login(usuario,password):
+    print('entro a CONSULTA_LOGIN')
     engine = conexion('administradores')
     metadata = MetaData()
-
     # Obtiene una tabla de la base de datos
     table = Table('lista', metadata, autoload=True, autoload_with=engine)
     
@@ -20,22 +18,21 @@ def consulta_login(usuario,password):
     # Ejecuta la consulta y obtiene el resultado
     result = engine.execute(query).fetchall()
     #print(type(result)) #es una lista
+    
     if result != '':
         for row in result:
             nombrebd_valor = row.data_base
             contra_valor = row.password
-        print('Nombre de la BD → ',nombrebd_valor)
-        bd_encriptado = set_password(nombrebd_valor)
-        print('BD encriptado → ',bd_encriptado)
-        #print(type(bd_encriptado))
+            usuario_valor = row.user
 
         password_encriptado = string_a_byte(contra_valor)
+        print(password_encriptado)
         #print('password_encriptado → ',password_encriptado)
         #password_encriptado = byte_a_string(contra_valor)
         #print('password_encriptado → ',password.encode('utf-8'))
         if bcrypt.checkpw(password.encode('utf-8'), password_encriptado):
             print('las contraseñas coinciden!!')
-            return True,bd_encriptado
+            return True,usuario_valor
         else:
             return False
     else : return False
@@ -44,3 +41,23 @@ def set_password(pw):
     pwhash = bcrypt.hashpw(pw.encode('utf-8'), bcrypt.gensalt())
     password_hash = pwhash.decode('utf-8') # decode the hash to prevent is encoded twice
     return password_hash
+
+def obtener_bd(user):
+    engine = conexion('administradores')
+    metadata = MetaData()
+    nombrebd_valor = ""
+    # Obtiene una tabla de la base de datos
+    table = Table('lista', metadata, autoload=True, autoload_with=engine)
+    
+    # Crea una consulta usando el objeto table
+    #query = (select([table]).where(table.columns.user == usuario) and (table.columns.password == password_encriptado))
+    query = (select([table]).where(table.columns.user == user))
+    # Ejecuta la consulta y obtiene el resultado
+    result = engine.execute(query).fetchall()
+    #print(type(result)) #es una lista
+    if result != '':
+        for row in result:
+            nombrebd_valor = row.data_base
+            id_admin = row.id
+    print('Nombre de la BD del usuario → ',nombrebd_valor)
+    return nombrebd_valor,id_admin
