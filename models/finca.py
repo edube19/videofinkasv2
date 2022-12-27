570f46e39a869a4d9b0811a7ff6aa1a34a5be1f1
edube19 <92874291+edube19@users.noreply.github.com>
2022-12-27 10:14:45 -0500

    Add files via upload

--- models/finca.py
+++ models/finca.py
@@ -6,16 +6,13 @@ class Finca(Base):
     __tablename__ = "finca"
     #__table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True,nullable=False)
-    admin_id= Column(Integer,nullable=False,unique=True)
     direccion= Column(String(45),nullable=False,unique=True)
     nombre= Column(String(45),nullable=False,unique=True)
     fecha_creacion= Column(String(45))
     fecha_modificacion= Column(String(45))
     total_porc_participacion= Column(Float(20),nullable=False)
 
-    def __init__(self, admin_id, direccion,nombre,fecha_creacion,fecha_modificacion,total_porc_participacion):
-        #self.id = id
-        self.admin_id = admin_id
+    def __init__(self,direccion,nombre,fecha_creacion,fecha_modificacion,total_porc_participacion):
         self.direccion = direccion
         self.nombre = nombre
         self.fecha_creacion = fecha_creacion
