872462223982ab117a3bddd358c5bd295dc08e73
edube19 <92874291+edube19@users.noreply.github.com>
2022-12-28 14:27:15 -0500

    Add files via upload

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -1,41 +1,42 @@
 from sqlalchemy import create_engine, MetaData, Table,select
-from re_excel import *
+from recursos.re_encriptacion import *
+from sqlalchemy.orm import sessionmaker
+
+def string_a_byte(palabra):#en string
+    palabra = str.encode(palabra)
+    return palabra
+
 def conexion(admin_name):
     engine = create_engine(f'mysql://root:admin@localhost/{admin_name}')
     # Obtiene información sobre la estructura de la base de datos
     return engine
 
 def consulta_login(usuario,password):
-    print('entro a CONSULTA_LOGIN')
-    engine = conexion('administradores')
-    metadata = MetaData()
-    # Obtiene una tabla de la base de datos
-    table = Table('lista', metadata, autoload=True, autoload_with=engine)
-    
-    # Crea una consulta usando el objeto table
-    #query = (select([table]).where(table.columns.user == usuario) and (table.columns.password == password_encriptado))
-    query = (select([table]).where(table.columns.user == usuario))
-    # Ejecuta la consulta y obtiene el resultado
-    result = engine.execute(query).fetchall()
-    #print(type(result)) #es una lista
-    
-    if result != '':
-        for row in result:
-            nombrebd_valor = row.data_base
-            contra_valor = row.password
-            usuario_valor = row.user
-
-        password_encriptado = string_a_byte(contra_valor)
-        print(password_encriptado)
-        #print('password_encriptado → ',password_encriptado)
-        #password_encriptado = byte_a_string(contra_valor)
-        #print('password_encriptado → ',password.encode('utf-8'))
-        if bcrypt.checkpw(password.encode('utf-8'), password_encriptado):
-            print('las contraseñas coinciden!!')
-            return True,usuario_valor
-        else:
-            return False
-    else : return False
+    try : 
+        engine = conexion('administradores')
+        metadata = MetaData()
+        # Obtiene una tabla de la base de datos
+        table = Table('lista', metadata, autoload=True, autoload_with=engine)
+        
+        # Crea una consulta usando el objeto table
+        #query = (select([table]).where(table.columns.user == usuario) and (table.columns.password == password_encriptado))
+        query = (select([table]).where(table.columns.user == usuario))
+        # Ejecuta la consulta y obtiene el resultado
+        result = engine.execute(query).fetchall()
+        usuario_valor = ''
+        if result != '':
+            for row in result:
+                contra_valor = row.password
+                usuario_valor = row.user
+            password_encriptado = string_a_byte(contra_valor)
+            if bcrypt.checkpw(password.encode('utf-8'), password_encriptado):
+                print('las contraseñas coinciden!!')
+                return True,usuario_valor
+            else: return False,usuario_valor
+        else : return False,usuario_valor
+    except Exception as e : 
+        print('Error en la consulta de la BD → '+str(e))
+        return False
 
 def set_password(pw):
     pwhash = bcrypt.hashpw(pw.encode('utf-8'), bcrypt.gensalt())
@@ -58,6 +59,12 @@ def obtener_bd(user):
     if result != '':
         for row in result:
             nombrebd_valor = row.data_base
-            id_admin = row.id
+
     print('Nombre de la BD del usuario → ',nombrebd_valor)
-    return nombrebd_valor,id_admin
+    return nombrebd_valor
+
+def get_sesion(admin_name):
+    engine = create_engine(f'mysql://root:admin@localhost/{admin_name}')
+    Session = sessionmaker(bind= engine)
+    session = Session()
+    return session,engine
